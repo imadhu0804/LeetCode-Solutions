316241e4cf5f6f9882dcf9b77bed0729974be147
imadhu0804 <madhubabukotapati5@gmail.com>
2025-07-07 16:14:44 +0530

    Create 2114_MaximumNumberOfWordsFoundInSentences.java

---
+++ Easy/2114_MaximumNumberOfWordsFoundInSentences.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int mostWordsFound(String[] sentences) {
+        int maxWords=0;
+        for(String sentence : sentences){
+            int wordCount=sentence.split(" ").length;
+            if(wordCount>maxWords){
+                maxWords=wordCount;
+            }
+        }
+        return maxWords;
+    }
+}
