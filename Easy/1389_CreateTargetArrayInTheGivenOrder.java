0eb9e2984175f486a669165353ffd5e2434ed611
Madhubabu Kotapati <madhubabukotapati5@gmail.com>
2025-07-18 14:19:33 +0530

    Create 1389_CreateTargetArrayInTheGivenOrder.java

---
+++ Easy/1389_CreateTargetArrayInTheGivenOrder.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Solution {
+    public static int[] createTargetArray(int[] nums, int[] index) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            list.add(index[i], nums[i]);
+        }
+        int[] result = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            result[i] = list.get(i);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 2, 3, 4};
+        int[] index = {0, 1, 2, 2, 1};
+        System.out.println("Result: " + Arrays.toString(createTargetArray(nums, index)));
+    }
+}
